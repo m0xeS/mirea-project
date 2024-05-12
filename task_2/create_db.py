c1c3b1e6d581eddfac14925a819e2f85a1ccd120
m0xeS <70742783+m0xeS@users.noreply.github.com>
2024-05-12 14:32:48 +0300

    Update create_db.py

--- task_2/create_db.py
+++ task_2/create_db.py
@@ -57,27 +57,3 @@ finally:
     # Закрытие соединения
     if conn:
         conn.close()
-
-def print_table(rows, header):
-    print("|".join(header))
-    print("-" * (len(header)*20))
-    for row in rows:
-        print("|".join(str(cell) for cell in row))
-
-# Создание соединения с базой данных
-conn = sqlite3.connect('students.db')
-
-# Создание курсора для выполнения операций с базой данных
-cursor = conn.cursor()
-
-# Запрос на выборку всех дат экзаменов
-cursor.execute("SELECT exam_date FROM records")
-
-# Получение результатов запроса
-exam_dates = cursor.fetchall()
-
-# Вывод результатов в виде таблицы
-print_table(exam_dates, ["Дата экзамена"])
-
-# Закрытие соединения
-conn.close()
